81db5dd1e611dc0470a569f27857a3237dcb7327
hrl7 <hrlclb@gmail.com>
2016-01-21 16:32:13 +0900

    Add comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120141623) do
+ActiveRecord::Schema.define(version: 20160121071348) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.string   "attach"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
